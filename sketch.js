09147fd11874d1462bf9f9397b188d94d67ad0df
winnie-woo <hanxuwu0428@gmail.com>
2022-12-15 23:36:25 -0800

    update

--- sketch.js
+++ sketch.js
@@ -25,6 +25,7 @@ let animating = false;
 let cutedogs = [];
 let imageCounter= 0;
 let button;
+let cnv;
 
 
 function preload(){
@@ -34,7 +35,8 @@ for (let i = 0; i<= 11; i++) {
 } 
 
 function setup() {
-    createCanvas(600, 600);
+    cnv = createCanvas(1300, 700);
+    cnv.parent("#canvasDiv");
     background(255, 240, 179);
     textSize(36);
     textFont('courier new');
@@ -42,13 +44,16 @@ function setup() {
     textStyle(BOLD);
     fill(255);
     imageMode(CENTER);
-    frameRate(8); 
+    frameRate(6); 
+    
     
     text("click to randomizer",width/2,height/2);
     console.log(cutedogs);
 
-    button = createButton("click to see your dog");
-    button.mousePressed(buttonPressed)
+    //button = createButton("click to see your dog");
+    button= select('#randButton');
+    button.mousePressed(buttonPressed);
+    button.class("randomizerButton");
   }
   
 
@@ -62,13 +67,13 @@ function setup() {
       if (imageCounter  < cutedogs.length - 1){
         imageCounter++; 
         console.log(imageCounter);
-    }  else{
-      imageCounter=0; 
+    }  else{ 
+      imageCounter= 0; 
     }
 
   }
   }
-
+ 
   
 function changeBackground(){
   if (counter<= 6){
@@ -84,7 +89,7 @@ function randomizer(){
   animating = false;
  if (Bunnies [0]){
  //background(random(200,255));
-
+ clear();
  randomIndex= int(random(Bunnies.Length));
  text(Bunnies[randomIndex].name,width/2,height-25);
  image(random(cutedogs),width/2,height/2);
